6abbefd2b348969d48cb50b7929284f07ec9cd3d
AhaD <abuahad35188@gmail.com>
2023-05-23 23:06:37 +0600

    menucategory setup

--- src/Pages/Menu/Menu/Menu.jsx
+++ src/Pages/Menu/Menu/Menu.jsx
@@ -1,15 +1,35 @@
 import { Helmet } from "react-helmet-async";
 import Cover from "../../Shared/Cover/Cover";
 import menuImg from '../../../assets/menu/banner3.jpg'
-import PopularMenu from "../../Home/PopularMenu/PopularMenu";
+import dessertImg from '../../../assets/menu/dessert-bg.jpeg'
+import pizzaImg from '../../../assets/menu/pizza-bg.jpg'
+import saladImg from '../../../assets/menu/salad-bg.jpg'
+import soupImg from '../../../assets/menu/soup-bg.jpg'
+import useMenu from "../../../hookes/useMenu";
+import SectionTitle from "../../../Components/SectionTitle/SectionTitle";
+import MenuCategory from "../MenuCategory/MenuCategory";
 const Menu = () => {
+     const [menu] = useMenu()
+     const desserts = menu.filter(item => item.category === 'dessert');
+     const soup = menu.filter(item => item.category === 'soup');
+     const salad = menu.filter(item => item.category === 'salad');
+     const pizza = menu.filter(item => item.category === 'pizza');
+     const offered = menu.filter(item => item.category === 'offered');
   return (
     <div>
       <Helmet>
         <title>Bistro Boss | Menu</title>
       </Helmet>
           <Cover img={menuImg} title="Our Menu"></Cover>
-           <PopularMenu></PopularMenu>
+           {/* main cover */}
+           <SectionTitle subHeading="Don't Miss" heading="Today's Offer"></SectionTitle>
+            {/* offered menu items */}
+            <MenuCategory items={offered}></MenuCategory>
+              {/* dessert menu items  */}
+            <MenuCategory items={desserts} title="dessert" img={dessertImg}></MenuCategory>
+            <MenuCategory items={pizza} title={"pizza"} img={pizzaImg}></MenuCategory>
+            <MenuCategory items={salad} title={"salad"} img={saladImg}></MenuCategory>
+            <MenuCategory items={soup} title={"soup"} img={soupImg}></MenuCategory>
            
            
       <h1>Men men menu</h1>
