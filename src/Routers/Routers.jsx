e6e90bfbda91c755a4cbc9032093524488d89097
AhaD <abuahad35188@gmail.com>
2023-05-28 23:21:55 +0600

    singup page creat a user for data base

--- src/Routers/Routers.jsx
+++ src/Routers/Routers.jsx
@@ -44,7 +44,7 @@ export const router = createBrowserRouter([
      },
      {
           path:'dashboard',
-          element:<Dashboard></Dashboard>,
+          element:<PrivateRoute><Dashboard></Dashboard></PrivateRoute>,
           children:[
                {
                     path:'mycart',
