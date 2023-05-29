a9337002afa6e2bdcb52bb2e06f84300f0191b11
AhaD <abuahad35188@gmail.com>
2023-05-29 10:54:12 +0600

    jwt token create AuthProvider

--- src/Provider/AuthProvider.jsx
+++ src/Provider/AuthProvider.jsx
@@ -1,6 +1,7 @@
 import { createContext, useEffect, useState } from "react";
 import { GoogleAuthProvider, createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from "firebase/auth";
 import { app } from "../Firebase/Firebase.config";
+import axios, { Axios } from "axios";
  
 
 export const AuthContext = createContext(null)
@@ -39,7 +40,19 @@ const AuthProvider = ({children}) => {
      useEffect(() => {
           const unsubscribe = onAuthStateChanged(auth, currentUser => {
               setUser(currentUser);
-              console.log('current user', currentUser);
+          //     console.log('current user', currentUser);
+          // get and set token
+          if(currentUser){
+               axios.post('http://localhost:5000/jwt',{email: currentUser.email})
+               .then(data=>{
+                    console.log(data.data.token);
+                    localStorage.setItem('Token', data.data.token)
+               })
+          }else{
+               localStorage.removeItem('Token')
+          }
+
+
               setLoading(false);
           });
           return () => {
