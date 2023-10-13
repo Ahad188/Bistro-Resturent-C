0b6e7f4013591f1b619474970408c484c9592bf2
AhaD <abuahad35188@gmail.com>
2023-10-13 18:55:22 +0600

    contact ande bisto bos component responsive

--- src/Pages/Home/BistroBoss/BistroBoss.jsx
+++ src/Pages/Home/BistroBoss/BistroBoss.jsx
@@ -3,7 +3,7 @@
 const BistroBoss = () => {
      return (
           <div className='bistro h-[600px] bg-fixed relative text-center'>
-               <div className='w-[750px] p-20  absolute top-32 left-80 bg-white'>
+               <div className='md:w-[750px] p-20  md:absolute top-32 left-80 bg-white'>
                     <h3 className='uppercase text-4xl my-6'>Bistro Boss</h3>
                     <p className='text-center text-black text-xl'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat temporibus numquam quae libero possimus inventore quod facere veniam ullam quasi, alias veritatis nobis, eveniet magni?</p>
                </div>
--- src/Pages/Home/Contact/Contact.jsx
+++ src/Pages/Home/Contact/Contact.jsx
@@ -3,7 +3,7 @@
 const Contact = () => {
      return (
           <div className="bg-black h-[350px] mt-10 mb-10">
-               <h3 className="text-red text-5xl text-center p-40 text-indigo-300">Call Us :983791219230973</h3>
+               <h3 className="text-red md:text-5xl text-center p-40 text-indigo-300">Call Us :983791219230973</h3>
           </div>
      );
 };
